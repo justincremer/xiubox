9e14f53b14752c5e483454fe16e43a616f980349
justincremer <jacremer@live.com>
2021-04-20 01:41:50 -0700

    intial client workflow

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
 	return (
 		<div className="container">
 			<h2>Home page</h2>
-			<button className="bg-blue-500 hover:bg-blue-200 text-white font-bold py-2 px-4 rounded">
+			<button className="bg-blue-500 hover:bg-blue-200 text-white font-bold py-2 px-4">
 				test
 			</button>
 		</div>
